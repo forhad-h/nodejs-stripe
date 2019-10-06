354c7daecac9e39afb74160ffaceb6b2b56ea1f8
forhad-h <hosainf1@gmail.com>
2019-10-06 06:06:27 +0600

    some refactoring

---
+++ public/front-end-only-client.js
@@ -0,0 +1,15 @@
+var stripe = Stripe('publishable-key')
+var checkoutButton = document.querySelector('#checkout-button')
+checkoutButton.addEventListener('click', function () {
+  stripe.redirectToCheckout({
+    items: [
+      {
+        // to get 'sku' add product in stripe account dashboard
+        sku: 'sku_FwEhYOP8NY6Eav',
+        quantity: 1
+      }
+    ],
+    successUrl: 'http://localhost:8000/success',
+    cancelUrl: 'http://localhost:8000/cancel'
+  })
+})
---
+++ public/legacy-version-client.js
@@ -0,0 +1,34 @@
+var checkoutHandler = StripeCheckout.configure({
+  // Publishable API key
+  key: 'publishable-key',
+  locale: 'auto'
+})
+var button = document.getElementById('buttonCheckout')
+button.addEventListener('click', function (ev) {
+  checkoutHandler.open({
+    name: 'Sample Store',
+    description: 'Example Purchase',
+    token: handleToken
+  })
+})
+
+function handleToken (token) {
+  fetch('/charge', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(token)
+  })
+    .then(response => {
+      if (!response.ok) throw response
+      return response.json()
+    })
+    .then(output => {
+      console.log('Purchase succeeded', output)
+      if (output.status === 'succeeded') {
+        document.getElementById('shop').innerHTML = '<p>Purchase complete!</p>'
+      }
+    })
+    .catch(err => {
+      console.log('Purchase failed', err)
+    })
+}
